a6cb06f16c003ad8c754d7e442889bc1ef7b3a3a
Dakouri  Kobri <maurille.kobri@gmail.com>
2021-04-06 02:53:44 +0200

    1. Add metadata link for view port in index.html; 2. Add TrackingSummary component; 3. Add CSS rules for and updated  List and TrackingSummary components

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <div className="App">
       <Header />
-      <List />
+      {/* <List /> */}
       <TrackingSummary />
       <Footer />
     </div>
--- src/components/TrackingSummary.jsx
+++ src/components/TrackingSummary.jsx
@@ -1,7 +1,22 @@
 export default function TrackingSummary(){
     return(
-        <div>
-            <h2> Here goes detailed information of each package.</h2>
+        <div className="tracking-summary">
+            <h1>Tracking Summary</h1>
+
+{/* Build table for single package tracking summary */}
+<table className="list-table">
+  <thead>
+    <tr>
+      <th colspan="2">Package # 1</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Delivered</td>
+      <td>2020-09-15T05:51:13Z</td>
+    </tr>
+  </tbody>
+</table>
         </div>
     );
 }
